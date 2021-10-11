62b3e9da831e22d55663045967c61a90c027552e
sapargaliyevasb <89898671+sapargaliyevasb@users.noreply.github.com>
2021-10-11 23:55:28 +0600

    Create gen.js

---
+++ gen.js
@@ -0,0 +1,7 @@
+var gen_seq = document.getElementById("gen_seq").value;
+var amino_seq = document.getElementById("amino_seq").value;
+var standard = document.getElementById("standard").value;
+
+if (gen_seq.length < 1) {
+    alert("Incorrect input. Try again!");
+  }
